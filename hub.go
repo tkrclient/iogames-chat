f969418907d518d9c68491061c614da3d7ce78a7
script26 <johnadams102@posteo.de>
2025-03-18 20:24:34 -0700

    format code tabs properly

--- hub.go
+++ hub.go
@@ -32,18 +32,18 @@ func newHub() *Hub {
 func (h *Hub) run() {
 	for {
 		select {
-		case client := <-h.register:
+			case client := <-h.register:
 			h.clients[client] = true
-		case client := <-h.unregister:
+			case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
 			}
-		case message := <-h.broadcast:
+			case message := <-h.broadcast:
 			for client := range h.clients {
 				select {
-				case client.send <- message:
-				default:
+					case client.send <- message:
+					default:
 					close(client.send)
 					delete(h.clients, client)
 				}
