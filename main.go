006b0e52d0ddf22532ca7c00197deea8965638b4
script26 <johnadams102@posteo.de>
2025-03-18 20:21:41 -0700

    format code tabs properly

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 	// Define the address and directory to serve
 	addr := flag.String("addr", ":8080", "http service address")
 	dir := flag.String("dir", ".", "the directory to serve files from")
-    hub := newHub()
-    go hub.run()
+	hub := newHub()
+	go hub.run()
 	flag.Parse()
 
 	// Create a file server to serve files from the specified directory
@@ -24,10 +24,10 @@ func main() {
 	// Handle "/" to serve all files in the directory
 	http.Handle("/", fileServer)
 
-    // Handle "/ws" for WebSocket connections
-    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-        serveWs(hub, w, r)
-    })
+	// Handle "/ws" for WebSocket connections
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
 
 	// Start the HTTP server
 	log.Printf("Serving files from %s on HTTP port: %s\n", *dir, *addr)
